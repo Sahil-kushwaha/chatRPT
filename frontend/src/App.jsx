2963011a27edbf72151f5670e629433a40b03ff6
sahil kushwaha <sahilkushwaha5510@gmail.com>
2025-12-04 16:56:15 +0530

    frontend with react initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='border'>helolp</div>
+    </>
+  )
+}
+
+export default App
