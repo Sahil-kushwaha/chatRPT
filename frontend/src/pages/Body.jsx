1cd724fe9f41d186d9db5816ef5198dae91bd5dc
sahil kushwaha <sahilkushwaha5510@gmail.com>
2026-01-03 14:06:16 +0530

    feat: add lucide-react for icons and enhance Body component with copy functionality

--- frontend/src/pages/Body.jsx
+++ frontend/src/pages/Body.jsx
@@ -1,11 +1,21 @@
 import { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
+import {Check, Copy, SendHorizontal} from "lucide-react"
+
 function Body() {
   
   const [ conversations,setConversations] = useState([])
   const [userQuery, setUserQuery]  = useState("")
-  const [isProcess ,setIsProcess] = useState(true)
+  const [isProcess ,setIsProcess] = useState(false)
+  const [isAssistanceContentCopyToClipboard ,setIsAssistanceContentCopyToClipboard] = useState({})
+  const [isUserContentCopyToClipboard ,setIsUserContentCopyToClipboard] = useState({})
+  const [isUserContentHover ,setIsUserContentHover] = useState({})
   const scrollRef = useRef(null)
+  const threadId = useRef(null)
+
+ useEffect(()=>{
+   threadId.current = Date.now().toString(36)+Math.random().toString(36).substring(2,9) 
+ },[])
 
   const generate = async(text)=>{
     // show userInput on UI
@@ -28,14 +38,12 @@ function Body() {
           "Content-Type":"application/json"
          },
          signal:abtCtrl.signal,
-         body:JSON.stringify({message:text})
+         body:JSON.stringify({threadId:threadId.current,message:text})
        }
        )
        clearTimeout(reqTimeout)
        const jsonRes = await response.json()
-       console.log(jsonRes)
        const llmRes = {role:"assistance" ,content:jsonRes.data}
-       console.log(llmRes)
        // show result on UI
        setConversations(prev => [...prev ,llmRes])
        setIsProcess(false)
@@ -45,8 +53,6 @@ function Body() {
    }
        
   }
-
-
 
   const handleEnter = (e)=>{
      if(!userQuery.trim()) return
@@ -62,6 +68,26 @@ function Body() {
     scrollRef.current.scrollIntoView()
   },[conversations])
 
+  const handleCopyTextToClipboard = async (content,index ,fn)=>{
+      try {
+           await navigator.clipboard.writeText(content)
+           fn((prev)=>({...prev,[index]:true}))
+           setTimeout(()=>{
+              fn(false)
+           },2000)
+      } catch (error) {
+        console.error("failed to copy text",error)
+      }
+  }
+
+  const handleMouseHover =(index)=>{
+      setIsUserContentHover(prev=>({...prev,[index]:true}))
+    }
+    const handleMouseLeave =(index)=>{
+      setTimeout(()=>{
+        setIsUserContentHover(prev=>({...prev,[index]:false}))        
+      },3000)
+  }
   return (
     <div className="dark:bg-background-dark dark:text-text-dark bg-background-light  text-text-light min-h-screen">
       <div className="flex flex-col">
@@ -69,13 +95,37 @@ function Body() {
           <main className="max-w-3xl flex-1 self-center w-full pb-32 mt-6 ">
             {conversations.map((item,index)=>{
                if(item.role==="user"){
-                  return <div key={index} className="bg-neutral-300 dark:bg-neutral-700 rounded-l-xl rounded-r-xl p-2 m-4 max-w-fit ml-auto">
+                  return(
+                    <div key={index} 
+                      className="bg-neutral-300 dark:bg-neutral-700 rounded-l-xl rounded-r-xl p-2 m-4 max-w-fit ml-auto relative"
+                      onMouseEnter={()=>handleMouseHover(index)}
+                      onMouseLeave={()=>handleMouseLeave(index)}
+                    >
                       {item.content}
-                   </div>
+
+                     {
+                       isUserContentHover[index]?
+                       <div
+                        className="cursor-pointer  max-w-fit absolute right-0 -bottom-6 "
+                        title="copy" 
+                        onClick={()=>handleCopyTextToClipboard(item.content,index ,setIsUserContentCopyToClipboard)}
+                       >
+                         {isUserContentCopyToClipboard[index]?<Check size={16} strokeWidth={1.75} />:<Copy size={16} strokeWidth={1.75} />}
+                       </div>:""
+                     }   
+                    </div>
+                  )
                }
-                  return <div key={index} className="bg-neutral-300 dark:bg-neutral-800 text-text-light dark:text-neutral-300  rounded-l-xl rounded-r-xl p-2 m-4 max-w-fit" >
+                  return( 
+                    <div key={index} className="bg-neutral-300 dark:bg-neutral-800 text-text-light dark:text-neutral-300  rounded-l-xl rounded-r-xl p-2 m-4 max-w-fit" >
                         {item.content}
-                  </div>
+                        <div
+                        className="cursor-pointer pt-2 " 
+                        onClick={()=>handleCopyTextToClipboard(item.content,index,setIsAssistanceContentCopyToClipboard)}
+                        >
+                          {isAssistanceContentCopyToClipboard[index]?<Check size={16} strokeWidth={1.75} />:<Copy size={16} strokeWidth={1.75} />}
+                        </div>
+                    </div>)
             })}
             {isProcess?<div className="w-2 h-2 rounded-full bg-neutral-800 dark:bg-neutral-100 animate-ping m-4"></div>:"" }
             <div ref={scrollRef}></div>
@@ -95,7 +145,7 @@ function Body() {
                     <button 
                     className="mr-2 mb-2 bg-neutral-800 dark:bg-neutral-100 text-neutral-100 hover:opacity-80 cursor-pointer dark:text-neutral-900 self-end px-4 py-1 text-2xl   rounded-full"
                     onClick={handleSendBtn}
-                    >&#8593;</button>
+                    ><SendHorizontal /></button>
                   </div>
               </div>
             </div>
